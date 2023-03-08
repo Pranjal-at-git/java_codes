7676bdd5c6e849dce5781d265e126bd6dcf1351b
Pranjal-at-git <112642161+Pranjal-at-git@users.noreply.github.com>
2023-03-08 12:41:54 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,22 @@
+package pattern.program;
+
+public class pattern2 
+{
+	/*Left triangle in pattern*/
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=6;i++)
+		{
+			for(int j=1;j<=(6-i);j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) 
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+}
